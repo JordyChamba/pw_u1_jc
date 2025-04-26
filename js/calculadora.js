78c3b26306278386d7e948ffbc118e6753a57bb6
JordyChamba <jordy_chamba@hotmail.com>
2025-04-25 19:42:30 -0500

    Taller7

--- js/calculadora.js
+++ js/calculadora.js
@@ -138,4 +138,180 @@ const fundamentos = () => {
     const estudiantes=[estudiante11,estudiante12,estudiante13];
     console.table(estudiantes)
 
+    const ciudadano  = {
+        nombre: 'Jordy',
+        apellido: 'Chamba',
+        direccion: {
+            callePrincipal: 'Eloy Alfaro',
+            calleSecundario: 'De Las Orquideas',
+            referencia:'Al frente de la iglesia',
+            numeracion: '5620',
+            barrio: {
+                referencia: 'Al frente del estadio'
+            }
+        }
+    }
+    /* GET */ 
+    console.log(ciudadano);
+    console.log(ciudadano.nombre);
+    console.log(ciudadano.direccion);
+    console.log(ciudadano.direccion.callePrincipal);
+    console.log(ciudadano.direccion.barrio.referencia);
+
+    console.log(ciudadano.direccion.callePrincipal);
+
+    /* SET */ 
+    ciudadano.direccion.callePrincipal='Av.Amazonas';    
+    console.log(ciudadano.direccion.callePrincipal);
+
+    const arregloEstudiantes = [estudiante11,estudiante12];
+    console.log(arregloEstudiantes);
+
+    /*  Otra forma de hacer arreglos*/
+
+    const arregloEstudiantes2 = [{nombre:'Carla',apellido:'Castillo'}, 
+                                {nombre:'Lidia',apellido:'Teran'},
+    ];
+    console.log(arregloEstudiantes2);
+
+    /*Buscar la posocion del estudiante*/
+    console.log(arregloEstudiantes2[1].apellido)
+
+                        //Desestructuracion de Arreglos
+
+    /*Acceder a los elementos pero no por la posicion sino por la referencia 
+    (partir en variables partes) y asi acceder a varias partes */
+
+    const colores = ['Amarillo','Azul','Rojo','Verde','Naranja'];
+    const [c1,c2,c3,c4,c5] = colores;
+    console.log(c1); 
+    console.log(c5); 
+
+    const [cUno,cDos] = colores;
+    console.log(cUno);
+    console.log(cDos);
+
+    const [, ,cTres] = colores;
+    console.log(cTres);
+
+    const [c01,c02] = ['Amarillo','Azul','Rojo','Verde','Naranja'];
+    console.log(c01);
+    console.log(c02);
+
+    //manera1
+    desestrcuturar(colores);
+
+    //manera2
+    const valor1 = desestrcuturar2();
+    console.log(valor1);
+    
+    //manera3
+    const [,p2,p3, p4, p5, p6, p7, p8, p9, p10] = desestrcuturar2();
+    console.log(p3);
+    console.log(p2);
+    console.log(p10);
+
+    
+                         //Desestructuracion de Objetos
+    //manera1
+    const auto = {
+        marca: 'Toyota',
+        modelo: 'prius',
+        anio: 2020,
+        color: 'Amarillo'
+    }
+
+    const {marca, color,anio} = auto;
+    console.log(color);
+
+    //manera2
+    const {anio1} = {
+        marca1: 'Toyota',
+        modelo1: 'prius',
+        anio1: 2020,
+        color1: 'Amarillo'
+    }
+    console.log(anio1)
+    desestrcuturacionObjeto(auto);
+
+/* en arreglos se desestrcuturaba por la posicion y el obejtos por sus atributos */
+
+    const universidad = {
+        nombre:'UCE',
+        direccion: 'America',
+        rector:{
+            nombreR: 'Daniel',
+            apellido: 'Teran'
+        }
+    }
+
+    /*Buscar la posocion del Objeto UCE*/
+    //manera1
+    const {rector,nombreR}=universidad;
+    console.log(rector);
+    //manera2
+    const {apellido} = rector;
+    console.log(apellido);
+
+
+    const universidad2 = {
+        nombre2:'UCE',
+        direccion2: 'America',
+        rector2:{
+            nombreR2: 'Daniel',
+            apellido2: 'Teran'
+        }
+    }
+    const{rector2,nombreR2}=universidad2;
+    console.log(rector2);
+    const{apellido2}=rector2;
+    console.log(apellido2);
+
+                    //Desestrcuturacion por operador Res (Item de arreglos)
+    console.log('destructurar por operador rest');
+    const [p1,...resto]=desestrcuturar2();
+    console.log(p1);
+    console.log(resto);
+    /*Se destructura  la posicion 1 y el resto del arreglo */
+    
+
+                    //Desestrcuturacion por operador Res (Objetos)
+    //Desestrcuturar una parte de mi arreglo y el resto se mantenga igual
+    const autoR = {
+        marcaR: 'Toyota',
+        modeloR: 'prius',
+        anioR: 2020,
+        colorR: 'Amarillo'
+    }
+    const {marcaR,...restoO}=autoR;
+    console.log(marcaR);
+    console.log(restoO);
 };
+
+/* Puedo desestructurar en  cualquier parte del codigo 
+a cualquier variable que es un arreglo la puede desestrcuturar*/    
+
+function desestrcuturar([c1,c2,c3]){
+        console.log(c1);
+        console.log(c2);
+        console.log(c3);
+}
+
+function desestrcuturar2(){
+    const colores = ['Rosado','Violeta','Rojo','Cafe','Turquesa'];
+    return colores;
+}
+/* Como conclusion ma desestrcuturado de objeto que hace a traves de sus posiciones */
+
+
+//marnera1
+function desestrcuturacionObjeto(objeto){
+    console.log(objeto);
+}
+
+//manera2
+function desestrcuturacionObjeto({marca, color}){
+    console.log(marca);
+    console.log(color);
+}
+
